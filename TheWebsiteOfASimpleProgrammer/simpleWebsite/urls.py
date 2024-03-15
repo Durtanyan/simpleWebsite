8ba0f47a2ca74d5917583580c62d0f54bdd6be63
Durtanyan <lukins.ya@yandex.ru>
2024-03-15 20:56:47 +0300

    Динамические URL. Пользовательские конвертеры

--- TheWebsiteOfASimpleProgrammer/simpleWebsite/urls.py
+++ TheWebsiteOfASimpleProgrammer/simpleWebsite/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+from . import converter
+
+register_converter(converter.FourDigitalYearConverter, "year4")
 
 urlpatterns = [
 	path('', views.index),
-	path('cat/', views.categories),
+	path('cat/<int:cat_id>/', views.categories),
+	path('cat/<slug:cat_slug>/', views.categories_by_slug),
+	path('archive/<year4:year>/', views.archive)
 ]
--- TheWebsiteOfASimpleProgrammer/simpleWebsite/views.py
+++ TheWebsiteOfASimpleProgrammer/simpleWebsite/views.py
@@ -4,16 +4,44 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse('Первая страница сайта...')
 
-def categories(requests):
+def categories(requests, cat_id):
 	return HttpResponse("""<!DOCTYPE html>
-<html lang="ru">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Ты ж программист...</title>
-</head>
-<body>
-	<h1>Вторая страница сайта...</h1>
-	
-</body>
-</html>""")
+		<html lang="ru">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Ты ж программист...</title>
+		</head>
+		<body>
+			<h1>Вторая страница сайта...</h1>
+		</body>
+		</html>""")
+
+def categories_by_slug(requests, cat_slug):
+	return HttpResponse("""<!DOCTYPE html>
+		<html lang="ru">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Ты ж программист...</title>
+		</head>
+		<body>
+			<h1>Третья страница сайта...</h1>
+		</body>
+		</html>""")
+
+def archive(requests, year):
+	return HttpResponse("""
+		<!DOCTYPE html>
+		<html lang="ru">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Ты ж программист...</title>
+		</head>
+		<body>
+			<h1>Четвертая страница сайта...</h1>
+		</body>
+		</html>
+		""")
+
